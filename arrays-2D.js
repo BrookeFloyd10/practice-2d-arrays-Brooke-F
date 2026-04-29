9c5763129e9e4f44c509c3a088f4c984ced3f6bc
Brooke F <brooke.floyd10@gmail.com>
2026-04-29 12:28:12 -0500

    completed tasks 1, 2, and 3

---
+++ arrays-2D.js
@@ -0,0 +1,23 @@
+
+//Challenge 1: Student Scores
+let allScores = [
+    [98, 86, 99],
+    [65, 73, 80],
+    [82, 86, 88]
+];
+console.log(allScores);
+
+//Challenge 2: Build a Seating Grid
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"]
+];
+console.log(theaterSeating);
+
+// Challenge 3: Form a Multi-Dimensional Contact List
+let contactList = [
+    ["Brittani Frank", "BrittaniF@gmail.com", "573-334-5466"],
+    ["Gabriel Jones", "Jones.G@gmail.com", "816-859-9294"]
+];
+console.log(contactList);
